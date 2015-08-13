82f537114cb065898b98a67d0315f983e2b32850
James White <cyberyt@gmail.com>
2015-08-13 18:34:58 +0100

    Other files added, basic-op input get

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 console.log("lo Tosh got a Toshiba?");
 
-// 1. write an event listener on the basic-calc button
+// TICK 1. write an event listener on the basic-calc button
 // 2. inside the function on that event listener collect the values from the user and store them in variables
 // 3. do the relevant calculations on those variables
 // 4. show the answer div
@@ -15,9 +15,11 @@ basicButton.addEventListener('click', clicked);
 function clicked() {
   // debugger;
   // event.preventDefault();
-  basicNum1 = document.getElementById('basic-num-1').value;
-  basicNum2 = document.getElementById('basic-num-2').value;
+  basicNum1 = document.getElementById('basic-num-1').value; // sets basicNum vars to value of user inputs on 
+  basicNum2 = document.getElementById('basic-num-2').value; // boxes with IDs basic-num-1/2. 
+  basicOp = document.getElementById('basic-operation').value; // Gets value from dropdown, can return: +-*/
   console.log(basicNum1);
   console.log(basicNum2);
+  console.log(basicOp);
   console.log('clicked!');
 }
