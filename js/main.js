5539b441de2088b1f4ef458ced3765f69af0de77
James White <cyberyt@gmail.com>
2015-08-13 18:17:16 +0100

    can consolelog values from inputboxes

--- js/main.js
+++ js/main.js
@@ -7,9 +7,17 @@ console.log("lo Tosh got a Toshiba?");
 // 5. update the text of the answer div accordingly
 
 var basicButton = document.getElementById('basic-calc');
+var basicNum1;
+var basicNum2;
 
 basicButton.addEventListener('click', clicked);
 
 function clicked() {
+  // debugger;
+  // event.preventDefault();
+  basicNum1 = document.getElementById('basic-num-1').value;
+  basicNum2 = document.getElementById('basic-num-2').value;
+  console.log(basicNum1);
+  console.log(basicNum2);
   console.log('clicked!');
 }
