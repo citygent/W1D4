44d76fce0a0009f48bd6cd455a39bb545bd918de
James White <cyberyt@gmail.com>
2015-08-13 19:24:57 +0100

    Calculations can be performed

--- js/main.js
+++ js/main.js
@@ -1,25 +1,47 @@
 console.log("lo Tosh got a Toshiba?");
 
 // TICK 1. write an event listener on the basic-calc button
-// 2. inside the function on that event listener collect the values from the user and store them in variables
-// 3. do the relevant calculations on those variables
+// TICK 2. inside the function on that event listener collect the values from the user and store them in variables
+// TICK 3. do the relevant calculations on those variables
 // 4. show the answer div
 // 5. update the text of the answer div accordingly
 
 var basicButton = document.getElementById('basic-calc');
 var basicNum1;
 var basicNum2;
+var basicOp;
+var basicAns;
 
 basicButton.addEventListener('click', basicGet);
 
 function basicGet() {
   // debugger;
   // event.preventDefault();
-  basicNum1 = document.getElementById('basic-num-1').value; // sets basicNum vars to value of user inputs on 
-  basicNum2 = document.getElementById('basic-num-2').value; // boxes with IDs basic-num-1/2. 
   basicOp = document.getElementById('basic-operation').value; // Gets value from dropdown, can return: +-*/
-  console.log(basicNum1);
-  console.log(basicNum2);
-  console.log(basicOp);
-  console.log('clicked!');
+  basicNum1 = parseFloat(document.getElementById('basic-num-1').value); // Set basicNum vars to ParseFloated value of user inputs on 
+  basicNum2 = parseFloat(document.getElementById('basic-num-2').value); // input boxes with IDs basic-num-1/2. 
+  console.log(basicNum1); // REMOVE WHEN DONE
+  console.log(basicNum2); // REMOVE WHEN DONE
+  console.log(typeof basicNum1, typeof basicNum2); // REMOVE WHEN DONE
+  console.log('clicked!'); // REMOVE WHEN DONE
+    switch(basicOp) {
+      case"+":
+        basicAns = (basicNum1 + basicNum2) / 100; // /100 rounds to 2 decimal places. 
+        console.log(basicAns);
+        break;
+      case"-":
+        basicAns = (basicNum1 - basicNum2) / 100; // /100 rounds to 2 decimal places. 
+        console.log(basicAns);
+        break;
+      case"/":
+        basicAns = (basicNum1 / basicNum2) / 100; // /100 rounds to 2 decimal places. 
+        console.log(basicAns);
+        break;
+      case"*":
+        basicAns = (basicNum1 * basicNum2) / 100; // /100 rounds to 2 decimal places. 
+        console.log(basicAns);
+        break;      
+      default:
+        alert("How on earth did you manage that from a Dropdown?!"); // Developer humour is coming on better than the coding. 
+    }
 }
