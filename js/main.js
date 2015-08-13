f07a6cc1f431e79985de179f9eb369191be7991a
James White <cyberyt@gmail.com>
2015-08-13 17:27:14 +0100

    Added files, moved scipt tag, checked linked

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("lo Tosh got a Toshiba?");
+
+// 1. write an event listener on the basic-calc button
+// 2. inside the function on that event listener collect the values from the user and store them in variables
+// 3. do the relevant calculations on those variables
+// 4. show the answer div
+// 5. update the text of the answer div accordingly
