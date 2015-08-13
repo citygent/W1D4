348b48683b3f11d794955d49d37bc957faa4c919
James White <cyberyt@gmail.com>
2015-08-13 21:59:29 +0100

    imperial bmi works

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
 // TICK 3. do the relevant calculations on those variables
 // TICK 4. show the answer div
 // TICK 5. update the text of the answer div accordingly
+  // debugger;
+  // event.preventDefault();
 
 var basicButton = document.getElementById('basic-calc');
 var basicNum1; // input numbers
@@ -15,41 +17,78 @@ var basicAnsDisplay = document.getElementById('basic-answer-alert');
 basicButton.addEventListener('click', basicGet);
 
 function basicGet() {
-  // debugger;
-  // event.preventDefault();
   basicOp = document.getElementById('basic-operation').value; // Gets value from dropdown, can return: +-*/
   basicNum1 = parseFloat(document.getElementById('basic-num-1').value); // Set basicNum vars to ParseFloated value of user inputs on 
   basicNum2 = parseFloat(document.getElementById('basic-num-2').value); // input boxes with IDs basic-num-1/2. 
-  console.log(basicNum1); // REMOVE WHEN DONE
-  console.log(basicNum2); // REMOVE WHEN DONE
-  console.log(typeof basicNum1, typeof basicNum2); // REMOVE WHEN DONE
-  console.log('clicked!'); // REMOVE WHEN DONE
-    switch(basicOp) {
-      case"+":
-        basicAns = (basicNum1 + basicNum2).toFixed(2); //.toFixed(2) rounds float answers to 2 decimal places.
-        console.log(basicAns);
-        basicAnsBox.className = "show";
-        basicAnsDisplay.innerHTML = basicAns;
-        break;
-      case"-":
-        basicAns = (basicNum1 - basicNum2).toFixed(2); //.toFixed(2) rounds float answers to 2 decimal places.
-        console.log(basicAns);
-        basicAnsBox.className = "show";
-        basicAnsDisplay.innerHTML = basicAns;
-        break;
-      case"/":
-        basicAns = (basicNum1 / basicNum2).toFixed(2); //.toFixed(2) rounds float answers to 2 decimal places.
-        console.log(basicAns);
-        basicAnsBox.className = "show";
-        basicAnsDisplay.innerHTML = basicAns;
-        break;
-      case"*":
-        basicAns = (basicNum1 * basicNum2).toFixed(2); //.toFixed(2) rounds float answers to 2 decimal places.
-        console.log(basicAns);
-        basicAnsBox.className = "show";
-        basicAnsDisplay.innerHTML = basicAns;
-        break;      
-      default:
-        alert("How on earth did you manage that from a Dropdown?!"); // Developer humour is coming along better than the coding!
+  basicSum(basicNum1, basicNum2); // Calls the function that does the maths. 
+}
+
+function basicSum(basicNum1, basicNum2) {
+  switch(basicOp) {
+    case"+":
+      basicAns = (basicNum1 + basicNum2).toFixed(2); //.toFixed(2) rounds float answers to 2 decimal places.
+      console.log(basicAns);
+      basicAnsBox.className = "show";
+      basicAnsDisplay.innerHTML = basicAns;
+      break;
+    case"-":
+      basicAns = (basicNum1 - basicNum2).toFixed(2); //.toFixed(2) rounds float answers to 2 decimal places.
+      console.log(basicAns);
+      basicAnsBox.className = "show";
+      basicAnsDisplay.innerHTML = basicAns;
+      break;
+    case"/":
+      basicAns = (basicNum1 / basicNum2).toFixed(2); //.toFixed(2) rounds float answers to 2 decimal places.
+      console.log(basicAns);
+      basicAnsBox.className = "show";
+      basicAnsDisplay.innerHTML = basicAns;
+      break;
+    case"*":
+      basicAns = (basicNum1 * basicNum2).toFixed(2); //.toFixed(2) rounds float answers to 2 decimal places.
+      console.log(basicAns);
+      basicAnsBox.className = "show";
+      basicAnsDisplay.innerHTML = basicAns;
+      break;      
+    default:
+      alert("How on earth did you manage that from a Dropdown?!"); // Developer humour is coming along better than the coding!
+  }
+}
+
+var bmiButton = document.getElementById('bmi-calc'); // the button
+var bmiSelector = document.getElementById('bmi-units'); // the selector itself
+var bmiUnit; // the value the selector is on
+var bmiMassDisplay = document.getElementById('bmi-mass-unit'); // what unit the Mass is
+var bmiHeightDisplay = document.getElementById('bmi-height-unit'); // what unit the Height is
+var bmiMass = parseFloat(document.getElementById('bmi-mass').value);  // Sets bmiMass var to user input.
+var bmiHeight = parseFloat(document.getElementById('bmi-height').value); // Sets bmiHeight var to user input.
+var bmiAns;
+
+bmiSelector.addEventListener('change', bmiChange); // when selector changed, run bmiChange.
+bmiButton.addEventListener('click', bmiSum);  // when button is clicked do a thing.
+
+function bmiChange() {
+  bmiUnit = document.getElementById('bmi-units').value; // changes the value the selector is on
+    if (bmiUnit === 'imperial') {                           // IF TIME: Change values user imputs when selector is changed.
+      bmiMassDisplay.innerHTML = "lb";
+      bmiHeightDisplay.innerHTML = "in";
+      return bmiUnit;
     }
+    else if (bmiUnit === 'metric') {
+      bmiMassDisplay.innerHTML = "kg";
+      bmiHeightDisplay.innerHTML = "m";
+      return bmiUnit;
+    }
+}
+function bmiSum(bmiMass, bmiHeight) {
+  bmiMass = parseFloat(document.getElementById('bmi-mass').value);
+  bmiHeight = parseFloat(document.getElementById('bmi-height').value);
+  
+  if (bmiUnit === 'imperial') {
+    bmiAns = (bmiMass/(bmiHeight * bmiHeight)) * 703;
+    console.log(bmiAns);
+  }
+  else if (bmiUnit === 'metric') {
+    bmiAns = (bmiMass / Math.pow(bmiHeight, 2));
+    console.log(bmiAns);
+  }
 }
